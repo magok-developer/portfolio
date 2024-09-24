0197a0a730e5b84bb45cb83bf78e9e66d8e58bc8
Jiwon <hajw.study@gmail.com>
2024-09-24 22:31:50 +0900

    about me 부분 개발

---
+++ src/components/Main/AboutMe.tsx
@@ -0,0 +1,137 @@
+import { mainColor } from "@/styles/themes";
+import Image from "next/image";
+import React from "react";
+import styled from "styled-components";
+
+const AboutMe = () => {
+  return (
+    <Container>
+      <Intro>
+        <h1 className='text'>
+          FRONT-END<span>프론트엔드</span>
+        </h1>
+        <h1 className='text'>
+          DEVELOPER<span>개발자</span>
+        </h1>
+        <h1 className='text'>
+          HA JI WON<span>하지원 입니다.</span>
+        </h1>
+      </Intro>
+      <AboutMeWrap>
+        <LeftSection>
+          <div className='about-wrap'>
+            <span className='title'>Birth</span>
+            <span className='content'>1998.12.03</span>
+          </div>
+          <div className='about-wrap'>
+            <span className='title'>Email</span>
+            <span className='content'>hajw.study@gmail.com</span>
+          </div>
+          <div className='about-wrap'>
+            <span className='title'>Residence</span>
+            <span className='content'>Seoul, Korea</span>
+          </div>
+          <div className='about-wrap'>
+            <span className='title'>Completion</span>
+            <span className='content'>elice coding, SW Engineer Track 6</span>
+          </div>
+          <div className='about-wrap'>
+            <span className='title'>Career</span>
+            <span className='content'>
+              Terra International, Research Engineer
+            </span>
+          </div>
+        </LeftSection>
+
+        <img src='/image/증명사진.jpg' alt='photo' className='id-photo' />
+      </AboutMeWrap>
+    </Container>
+  );
+};
+
+export default AboutMe;
+
+const Container = styled.div`
+  margin-bottom: 80px;
+`;
+
+const Intro = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+
+  .text {
+    font-size: 2.4vw;
+    line-height: 100%;
+    margin: 0;
+
+    width: 20vw;
+    height: 5vh;
+
+    background-size: 0%;
+    transition: background-size cubic-bezier(0.1, 0.5, 0.5, 1) 0.5s;
+
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: center;
+    position: relative;
+  }
+
+  span {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background-color: ${mainColor.blue};
+
+    clip-path: polygon(0 50%, 100% 50%, 100% 50%, 0 50%);
+    transform-origin: center;
+    transition: all cubic-bezier(0.1, 0.5, 0.5, 1) 0.4s;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
+
+  .text:hover > span {
+    clip-path: polygon(0 0, 100% 0, 100% 100%, 0% 100%);
+  }
+`;
+
+const AboutMeWrap = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-end;
+  margin-top: 50px;
+
+  .id-photo {
+    width: 17%;
+    box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+  }
+`;
+
+const LeftSection = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  gap: 14px;
+
+  .about-wrap {
+    display: flex;
+    align-items: center;
+    gap: 20px;
+  }
+
+  .title {
+    color: ${mainColor.blue};
+    font-size: 1.5vw;
+    font-weight: bold;
+    width: 14vw;
+  }
+
+  .content {
+    font-size: 1.2vw;
+  }
+`;
