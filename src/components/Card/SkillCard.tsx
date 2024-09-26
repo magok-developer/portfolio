f115c717a7876a7c680582ded3c7565565ef905a
Jiwon <hajw.study@gmail.com>
2024-09-26 21:59:42 +0900

    skill card 컴포넌트 개발

---
+++ src/components/Card/SkillCard.tsx
@@ -0,0 +1,51 @@
+import Image from "next/image";
+import React from "react";
+import styled from "styled-components";
+
+type Props = {
+  data: { url: string; title: string; description: string };
+};
+const SkillCard = ({ data }: Props) => {
+  return (
+    <Container>
+      <img src={data.url} alt={data.title} />
+      <Description>{data.description}</Description>
+    </Container>
+  );
+};
+
+export default SkillCard;
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  width: 80px;
+  height: 80px;
+  background: transparent;
+  border: 0.5px solid rgba(255, 255, 255, 0.2);
+  box-shadow: 0px 10px 10px rgba(0, 0, 0, 0.2);
+  padding: 12px;
+  position: relative;
+  transition: width 0.3s ease-in-out, height 0.3s ease-in-out;
+
+  &:hover {
+    width: 120px;
+    height: 150px;
+    align-items: center;
+    justify-content: flex-start;
+  }
+
+  &:hover div {
+    display: block;
+    height: auto;
+  }
+`;
+
+const Description = styled.div`
+  margin-top: 8px;
+  font-size: 12px;
+  display: none;
+  text-align: center;
+`;
