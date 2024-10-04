b147106ca429874890d2c7cb2011616093752141
Jiwon <hajw.study@gmail.com>
2024-10-04 12:11:05 +0900

    google font 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,19 @@
 import RootContainer from "@/components/RootContainer";
 import "./globals.css";
+import { Noto_Sans_KR } from "next/font/google";
 
 export const metadata = {
   title: "Next.js App",
   description: "Next.js App with Light and Dark Theme",
 };
 
+const noto = Noto_Sans_KR({
+  subsets: ["latin"],
+});
+
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <html lang='en'>
+    <html lang='en' className={noto.className}>
       <body style={{ height: "100vh", position: "relative" }}>
         <RootContainer>{children}</RootContainer>
       </body>
