354624cde908b5da256843e8ea080d4ced14cb5b
JIWON <hajw.study@gmail.com>
2025-01-26 20:10:54 +0900

    project 컴포넌트 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,25 @@
 'use client';
 
 import AboutMe from '@/components/Main/AboutMe';
+import { IntroText } from '@/components/Text/IntroText';
+import Projects from '@/components/Main/Projects';
 import Stacks from '@/components/Main/Stacks';
-import Nav from '@/components/Nav/Nav';
 import React from 'react';
 import styled from 'styled-components';
+import ProjectFadeVersion from '@/components/Main/ProjectFadeVersion';
 
-const page = () => {
+const Page = () => {
   return (
     <Container>
+      <IntroText id="aboutMe" />
       <AboutMe />
-      <Stacks />
+      <Stacks id="stacks" />
+      <ProjectFadeVersion id="projects" />
     </Container>
   );
 };
 
-export default page;
+export default Page;
 
 const Container = styled.div`
   height: 100%;
